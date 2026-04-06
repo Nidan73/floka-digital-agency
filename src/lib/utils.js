897ac638e3ae7202ab264593aae8012fd0a5f34b
Nidan73 <Nidanalam73@gmail.com>
2026-04-06 07:08:30 +0600

    final commit

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Utility to merge Tailwind classes without conflicts.
- * Essential for premium components to stay clean.
- */
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
